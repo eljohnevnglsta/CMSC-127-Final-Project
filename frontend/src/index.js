53aec63a7842cb3bc61b2313c6a33b89654046f7
eljohnevnglsta <eyevangelista1@up.edu.ph>
2024-06-01 19:16:10 +0800

    Returned index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
 
-import EditReview from "./components/AddReview.js";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <App /> */}
-      <EditReview />
+      <App />
+      {/* <EditReview /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
