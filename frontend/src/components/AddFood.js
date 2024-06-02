b06873509ee9e6fb52d7f6166d109650bec3757c
elcartedave <daelcarte@up.edu.ph>
2024-06-02 22:50:35 +0800

    fix(added prompts when add food/establishment empty)

--- frontend/src/components/AddFood.js
+++ frontend/src/components/AddFood.js
@@ -1,118 +1,133 @@
 import { useEffect, useState } from "react";
 
-function AddFood (props) {
-    const username = props.username;
-    const show = props.show;
-    const businessid = props.businessid
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [foodtype, setFoodType] = useState([])
-    const [typeSelection, setFoodTypeSelection] = useState([])
+function AddFood(props) {
+  const username = props.username;
+  const show = props.show;
+  const businessid = props.businessid;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [foodtype, setFoodType] = useState([]);
+  const [typeSelection, setFoodTypeSelection] = useState([]);
 
+  useEffect(() => {
+    let url = "http://localhost:3001/select-type";
+    fetch(url)
+      .then((response) => response.json())
+      .then((body) => {
+        setFoodTypeSelection(body);
+        console.log(body);
+      });
+  }, []);
 
-    useEffect(() =>{
-        let url = "http://localhost:3001/select-type";
-        fetch(url)
-        .then((response) => response.json())
-        .then((body) => {
-            setFoodTypeSelection(body);
-            console.log(body)
-        });
-
-    }, [])
-
-    const handleCheckboxChange = (event) => {
-        const value = event.target.value;
-        setFoodType((prevFoodType) =>
-            prevFoodType.includes(value)
-                ? prevFoodType.filter((type) => type !== value)
-                : [...prevFoodType, value]
-        );
-    };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        
-        fetch('http://localhost:3001/food-item/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ 
-                username : username,
-                businessid: businessid,
-                name: name, 
-                price: price,
-                foodtype: foodtype,
-                
-            })
-        })
-        .then( () => {
-            setName('')
-            setPrice(0)
-            setFoodType([])
-            show(false) // Update state with the parsed data
-            window.location.reload()
-        });
-    };
-
-    function handleCancel () {
-        show(false)
-        // showReview(false)
-    }
-
-    return (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div className="bg-orange-50 rounded-lg p-6 w-full max-h-xl max-w-3xl">
-            <div className="flex justify-between items-center mb-4">
-            <div className='w-full'>
-            <h1 className='font-bold text-2xl mb-2 text-sky-950'>Add a Food </h1>
-        <form onSubmit={handleSubmit}>
-                <div className=' text-lg pb-2' >
-                <label className='font-bold mr-4'>Name:</label>
-                <input
-                    className='border-slate-300 rounded-lg border h-10 focus:shadow-md border-sky-950 pl-2 text-lg ml-4 w-11/12 text-sky-950'
-                    type="text" 
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </div>
-            <div className=' text-lg pb-2' >
-                <label className='font-bold mr-4'>Price:</label>
-                <input
-                    className='border-slate-300 rounded-lg border h-10 focus:shadow-md border-sky-950 pl-2 text-lg ml-4 w-11/12 text-sky-950'
-                    type="number"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                />
-            </div>
-            <div className=' text-lg pb-2' >
-                <label className='font-bold mr-4'>Food Types:</label>
-                <div className="pl-8">
-                <div className='grid lg:grid-cols-3 md:grid-cols-2  pb-6 mx-4'>
-                {typeSelection.map((type, index) => (
-                    <div key={index}>
-                        <input
-                            type="checkbox" 
-                            value={type.foodtype}
-                           
-                            onChange={handleCheckboxChange}
-                        />
-                        <label className='font-semibold mr-4' >{type.foodtype}</label>
-                    </div>
-                ))}
-                </div>
-                </div>
-            </div>
-            <button className='bg-sky-950 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-blue-950 ease-out duration-150' type="submit">Confirm</button>
-            <button  className='bg-red-800 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-red-950 ease-out duration-150' onClick={handleCancel}>Cancel</button>
-        </form>
-        
-        </div>
-        </div>
-        </div>
-        </div>
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    setFoodType((prevFoodType) =>
+      prevFoodType.includes(value)
+        ? prevFoodType.filter((type) => type !== value)
+        : [...prevFoodType, value]
     );
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!name || !price) {
+      alert("Food details incomplete!");
+      return;
+    }
+    if (foodtype.length === 0) {
+      alert("Food type is required");
+      return;
+    }
+    fetch("http://localhost:3001/food-item/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        businessid: businessid,
+        name: name,
+        price: price,
+        foodtype: foodtype,
+      }),
+    }).then(() => {
+      setName("");
+      setPrice(0);
+      setFoodType([]);
+      show(false); // Update state with the parsed data
+      window.location.reload();
+    });
+  };
+
+  function handleCancel() {
+    show(false);
+    // showReview(false)
+  }
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-orange-50 rounded-lg p-6 w-full max-h-xl max-w-3xl">
+        <div className="flex justify-between items-center mb-4">
+          <div className="w-full">
+            <h1 className="font-bold text-2xl mb-2 text-sky-950">
+              Add a Food{" "}
+            </h1>
+            <form onSubmit={handleSubmit}>
+              <div className=" text-lg pb-2">
+                <label className="font-bold mr-4">Name:</label>
+                <input
+                  className="border-slate-300 rounded-lg border h-10 focus:shadow-md border-sky-950 pl-2 text-lg ml-4 w-11/12 text-sky-950"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className=" text-lg pb-2">
+                <label className="font-bold mr-4">Price:</label>
+                <input
+                  className="border-slate-300 rounded-lg border h-10 focus:shadow-md border-sky-950 pl-2 text-lg ml-4 w-11/12 text-sky-950"
+                  type="number"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className=" text-lg pb-2">
+                <label className="font-bold mr-4">Food Types:</label>
+                <div className="pl-8">
+                  <div className="grid lg:grid-cols-3 md:grid-cols-2  pb-6 mx-4">
+                    {typeSelection.map((type, index) => (
+                      <div key={index}>
+                        <input
+                          type="checkbox"
+                          value={type.foodtype}
+                          onChange={handleCheckboxChange}
+                        />
+                        <label className="font-semibold mr-4">
+                          {type.foodtype}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <button
+                className="bg-sky-950 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-blue-950 ease-out duration-150"
+                type="submit"
+              >
+                Confirm
+              </button>
+              <button
+                className="bg-red-800 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-red-950 ease-out duration-150"
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddFood
+export default AddFood;
--- frontend/src/components/EditReview.js
+++ frontend/src/components/EditReview.js
@@ -1,140 +1,180 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 //using this component needs to pass (reviewid) as a prop
 
 const getReview = async (reviewid) => {
-    try {
-        const response = await fetch('http://localhost:3001/get-review', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ reviewid: reviewid })
-        });
-        const data = await response.json();
-        return data[0];
-    } catch (error) {
-        console.error('Error fetching review:', error);
-    }
-}
+  try {
+    const response = await fetch("http://localhost:3001/get-review", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ reviewid: reviewid }),
+    });
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error("Error fetching review:", error);
+  }
+};
 
 const getEstablishment = async (businessid) => {
-    try {
-        const response = await fetch('http://localhost:3001/get-business', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ businessid: businessid, searchCriteria: "businessid"})
-        });
-        const data = await response.json();
-        return data[0];
-    } catch (error) {
-        console.error('Error fetching establishment:', error);
-    }
-}
+  try {
+    const response = await fetch("http://localhost:3001/get-business", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        businessid: businessid,
+        searchCriteria: "businessid",
+      }),
+    });
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error("Error fetching establishment:", error);
+  }
+};
 
 const getFood = async (foodcode) => {
-    try {
-        const response = await fetch('http://localhost:3001/get-food', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ foodcode: foodcode, searchCriteria: "foodcode"})
-        });
-        const data = await response.json();
-        return data[0];
-    } catch (error) {
-        console.error('Error fetching food:', error);
-    }
-}
+  try {
+    const response = await fetch("http://localhost:3001/get-food", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ foodcode: foodcode, searchCriteria: "foodcode" }),
+    });
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error("Error fetching food:", error);
+  }
+};
 
 const handleSubmit = async (e, review, closeEdit) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const data = {
-        reviewid: review.reviewid,
-        content: e.target.review.value,
-        rating: e.target.rating.value,
-        username: localStorage.getItem('user')
-    };
+  const data = {
+    reviewid: review.reviewid,
+    content: e.target.review.value,
+    rating: e.target.rating.value,
+    username: localStorage.getItem("user"),
+  };
 
-    try {
-        const response = await fetch('http://localhost:3001/review/update', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        console.log('Review edited:', response);
-        closeEdit();
-        window.location.reload(); //hindi tama toh pero di ko alam kung pano tatawagin yung call kasi galing sya sa magkaibang render
-    } catch (error) {
-        console.error('Error editing review:', error);
-    }
-}
+  try {
+    const response = await fetch("http://localhost:3001/review/update", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log("Review edited:", response);
+    closeEdit();
+    window.location.reload(); //hindi tama toh pero di ko alam kung pano tatawagin yung call kasi galing sya sa magkaibang render
+  } catch (error) {
+    console.error("Error editing review:", error);
+  }
+};
 
 export default function EditReview({ reviewid, closeEdit }) {
-    const [review, setReview] = useState(null);
-    const [establishment, setEstablishment] = useState(null);
-    const [food, setFood] = useState(null);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const reviewData = await getReview(reviewid);
-            setReview(reviewData);
+  const [review, setReview] = useState(null);
+  const [establishment, setEstablishment] = useState(null);
+  const [food, setFood] = useState(null);
 
-            if (reviewData.reviewtype === 1) {
-                const establishmentData = await getEstablishment(reviewData.businessid);
-                setEstablishment(establishmentData);
-            } else {
-                const foodData = await getFood(reviewData.foodcode);
-                setFood(foodData);
-                const establishmentData = await getEstablishment(foodData.businessid);
-                setEstablishment(establishmentData);
-            }
-        };
-        fetchData();
-        
-    }, [reviewid]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const reviewData = await getReview(reviewid);
+      setReview(reviewData);
 
-    if (!review) {
-        return <div>Loading...</div>;
-    }
+      if (reviewData.reviewtype === 1) {
+        const establishmentData = await getEstablishment(reviewData.businessid);
+        setEstablishment(establishmentData);
+      } else {
+        const foodData = await getFood(reviewData.foodcode);
+        setFood(foodData);
+        const establishmentData = await getEstablishment(foodData.businessid);
+        setEstablishment(establishmentData);
+      }
+    };
+    fetchData();
+  }, [reviewid]);
 
-    
-    const reviewTypeText = review.reviewtype === 1 ? "Establishment" : "Food";
-    const reviewForText = review.reviewtype === 1 ? establishment?.name : `${establishment?.name}'s ${food?.name}`;
+  if (!review) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div className="bg-orange-50 rounded-lg p-6 w-full max-h-xl max-w-3xl">
-            <div className="flex justify-between items-center mb-4">
-            <div className='w-full'>
-            <h1 className='font-bold text-xl mb-2 text-sky-950'>Editing {reviewTypeText} Review</h1>
-            <p className='font-semibold text-lg mb-2 text-sky-950'>Review for {reviewForText}</p>
+  const reviewTypeText = review.reviewtype === 1 ? "Establishment" : "Food";
+  const reviewForText =
+    review.reviewtype === 1
+      ? establishment?.name
+      : `${establishment?.name}'s ${food?.name}`;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-orange-50 rounded-lg p-6 w-full max-h-xl max-w-3xl">
+        <div className="flex justify-between items-center mb-4">
+          <div className="w-full">
+            <h1 className="font-bold text-xl mb-2 text-sky-950">
+              Editing {reviewTypeText} Review
+            </h1>
+            <p className="font-semibold text-lg mb-2 text-sky-950">
+              Review for {reviewForText}
+            </p>
             <form onSubmit={(e) => handleSubmit(e, review, closeEdit)}>
-                <label className='font-semibold text-base mb-2 text-sky-950' htmlFor="rating">Rating:</label>
-                <select  className='border mr-1 ml-4 mb-2 py-1border-sky-950 rounded-full ' id="rating" name="rating" defaultValue={review.rating}>
-                    <option value="" disabled>Select a rating</option>
-                    {[1, 2, 3, 4, 5].map((rating) => (
-                        <option key={rating} value={rating}>
-                            {rating}
-                        </option>
-                    ))}
-                </select>
-                <br />
-                <label className='font-semibold text-base mb-2 text-sky-950' htmlFor="review">Content:</label>
-                <textarea className="w-full h-48 p-4 border mb-2 border-sky-950 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-950 focus:border-transparent text-base" id="review" name="review" defaultValue={review.content}></textarea>
-                <br />
-                <button className='bg-sky-950 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-blue-950 ease-out duration-150' type="submit">Submit</button>
-                <button className='bg-red-800 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-red-950 ease-out duration-150' onClick={() => closeEdit()}>Cancel</button>
+              <label
+                className="font-semibold text-base mb-2 text-sky-950"
+                htmlFor="rating"
+              >
+                Rating:
+              </label>
+              <select
+                className="border mr-1 ml-4 mb-2 py-1border-sky-950 rounded-full "
+                id="rating"
+                name="rating"
+                defaultValue={review.rating}
+              >
+                <option value="" disabled>
+                  Select a rating
+                </option>
+                {[1, 2, 3, 4, 5].map((rating) => (
+                  <option key={rating} value={rating}>
+                    {rating}
+                  </option>
+                ))}
+              </select>
+              <br />
+              <label
+                className="font-semibold text-base mb-2 text-sky-950"
+                htmlFor="review"
+              >
+                Content:
+              </label>
+              <textarea
+                className="w-full h-48 p-4 border mb-2 border-sky-950 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-950 focus:border-transparent text-base"
+                id="review"
+                name="review"
+                defaultValue={review.content}
+              ></textarea>
+              <br />
+              <button
+                className="bg-sky-950 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-blue-950 ease-out duration-150"
+                type="submit"
+              >
+                Submit
+              </button>
+              <button
+                className="bg-red-800 py-3 px-6 mx-2 rounded-full text-white transition hover:scale-105 hover:bg-red-950 ease-out duration-150"
+                onClick={() => closeEdit()}
+              >
+                Cancel
+              </button>
             </form>
-            </div>
+          </div>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
