877449c86f7ca4799300b0a61a9451b1cdb906b7
cazhiareese <159239010+cazhiareese@users.noreply.github.com>
2024-06-04 00:17:41 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
 import EditReview from "./components/EditReview.js";
 
-// import AddReview from "./components/AddReview.js";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      {/* <EditReview reviewid={10}/> */}
-      {/* <AddReview /> */}
+     
     </BrowserRouter> 
   </React.StrictMode>
 );
